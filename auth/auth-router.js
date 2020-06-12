22c256389b8bcc7863399ba64a25c498dc25ee4a
codecookride <57050292+codecookride@users.noreply.github.com>
2020-06-11 18:59:27 -0700

    done

---
+++ auth/auth-router.js
@@ -0,0 +1,76 @@
+const bcryptjs = require("bcryptjs");
+const router = require("express").Router();
+const jwt = require('jsonwebtoken');
+const secrets = require('./secrets');
+
+const Users = require("../user/user-model");
+const { isValid } = require("../user/user-services.js");
+
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+    credentials.password = hash;
+
+    Users.add(credentials)
+      .then(user => {
+        // const token = generateToken(saved);
+        res.status(201).json({ data: user});
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
+});
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({
+            message: "Welcome to our API",
+            token
+          });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+
+    department: user.role
+  };
+
+
+  const options = {
+    expiresIn: "2h"
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+module.exports = router;
---
+++ knexfile.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
+// if using a local postgres server, please create the database manually, Knex will not create it autmatically
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/auth.db3",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+ 
+  },
+
+  production: {
+    client: "pg",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+  
+  },
+};
